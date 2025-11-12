9fcb2bac35e8b5dd7841110ed79673f2680f6f58
goceterziev-creator <goce_terziev@abv.bg>
2025-11-12 08:55:29 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+// ⚙️ script.js
+class BookingSystem {
+    constructor() {
+        this.availableRooms = [];
+        this.bookings = [];
+    }
+    
+    checkAvailability(checkIn, checkOut) {
+        // Логика за проверка на наличност
+        return true;
+    }
+    
+    makeReservation(room, guest) {
+        // Логика за резервация
+        console.log(`Резервация за ${guest.name}`);
+    }
+}
